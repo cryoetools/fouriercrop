a4aeb12dde76d589c94b76d557e3ae14b7140ae4
cryoetools <cryoetools@outlook.com>
2025-02-24 12:06:17 +0800

    Add GUI.

--- src/fouriercrop/gui/main_window.py
+++ src/fouriercrop/gui/main_window.py
@@ -119,13 +119,15 @@ class App(customtkinter.CTk):
                     stderr=subprocess.PIPE,
                     shell=True,
                     text=True,
-                    preexec_fn=os.setsid,  # To enable process group termination
+                    preexec_fn=os.setsid,  # ignore [attr-defined]
                 )
 
                 while True:
                     if self.stop_event.is_set():
                         # Terminate the process group
-                        os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
+                        os.killpg(
+                            os.getpgid(self.process.pid), signal.SIGTERM
+                        )  # ignore [attr-defined]
                         append_text(textbox, "Command terminated by user.")
                         break
 
@@ -400,7 +402,7 @@ class App(customtkinter.CTk):
         """Handles the application's close event."""
         if self.process and self.process.poll() is None:
             if messagebox.askyesno("Exit Confirmation", "The program is running. Confirm exit?"):
-                os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
+                os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)  # ignore [attr-defined]
                 while True:
                     if self.process.poll() is not None:
                         self.destroy()
